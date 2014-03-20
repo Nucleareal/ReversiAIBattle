2a1b256b43a3ea4f067e55c7d189af7f104e2cf5
Nucleareal <nuclearfusionnuclearfusion@yahoo.co.jp>
2014-03-20 22:14:21 +0900

    Fixed not output "Debug" folder Bug

---
+++ Reversi/ReversiSystem/Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi.ReversiSystem.Debug
+{
+    class Logger
+    {
+        public static void String(bool b)
+        {
+            String(b.ToString());
+        }
+
+        public static void String(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
